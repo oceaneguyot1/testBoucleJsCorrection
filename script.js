2a6e8cd34da3133b6761c8acd3985ef3b4cf9d83
oceaneguyot1 <oceane.guyot@novei.fr>
2021-11-04 09:14:04 +0100

    Mise sous fonction du code précédent

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-//Variable avec le nombre 
-let number = 5;
+//fonction compteur pour afficher tous les nombres qui en précède un
+function compteur() {
+    //Variable avec le nombre 
+    let number = 5;
 
-//boucle
-for(let i=0; i<= number; i++){
-    console.log(i);
+    //boucle
+    for (let i = 0; i <= number; i++) {
+        console.log(i);
+    }
 }
+
+
+//appel de la fonction au chargement de la page
+compteur();
+
+
