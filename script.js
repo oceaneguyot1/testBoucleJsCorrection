a87633153a931dfe039b910f11c9f92143c033d2
oceaneguyot1 <oceane.guyot@novei.fr>
2021-11-04 09:16:32 +0100

    Récupération de la variable de l'input type number

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //fonction compteur pour afficher tous les nombres qui en précède un
 function compteur() {
-    //Variable avec le nombre 
-    let number = 5;
+    //Variable avec le nombre récupéré de l'input type number
+    let number = document.getElementById('number').value;
 
     //boucle
     for (let i = 0; i <= number; i++) {
